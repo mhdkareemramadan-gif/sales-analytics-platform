fc508c2d7cd2fe19fcfa17dda2a4b72e2762dbd0
Kareem Ramadan <mhdkareemramadan@gmail.com>
2026-02-06 10:15:23 +0100

    create data structure

---
+++ sales_analytics/main.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv("data/sales_data.csv")
+print(df.head())
+
+def main():
+    print("Project booted ✅")
+
+if __name__ == "__main__":
+    main()
